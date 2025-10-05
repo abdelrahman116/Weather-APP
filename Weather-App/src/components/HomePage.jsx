fb41ec71322341693c9d04511834e23f7eec810d
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-10-05 23:45:29 +0300

    Readme

---
+++ Weather-App/src/components/HomePage.jsx
@@ -0,0 +1,15 @@
+import Coolphone from "../assets/Cool/640.jpg";
+import Cooltap from "../assets/Cool/1200.jpg";
+import Cooldesk from "../assets/Cool/1920.jpg";
+import Hotdesk from "../assets/Sunny/1920x1080/1920.jpg";
+import Hottap from "../assets/Sunny/1280/1280.jpg";
+import Hotphone from "../assets/Sunny/640/640.jpg";
+import { useState } from "react";
+export default function HomePage() {
+  const [weather, setWeather] = useState("");
+  return (
+    <>
+      <div className="h-full w-full">Hi</div>
+    </>
+  );
+}
