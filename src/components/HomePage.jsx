a50bfeb9bd2a469d3d745c3a6123e4fc62f0178d
Abdelrahman Dawoud <112118879+abdelrahman116@users.noreply.github.com>
2025-10-16 20:41:40 +0300

    Change time format to 12-hour in HomePage component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -41,7 +41,7 @@ export default function HomePage() {
         const localTimeString = localDateObj.toLocaleTimeString("en-US", {
           hour: "2-digit",
           minute: "2-digit",
-          hour12: false,
+          hour12: true,
         });
 
         const localDayString = localDateObj.toLocaleDateString("en-US", {
