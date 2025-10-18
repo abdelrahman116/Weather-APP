d1da4ea452f54ad1763df553160542b861e90ee8
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-10-18 18:00:31 +0300

    Fixing the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-full text-center text-gray-400 text-sm mt-8 py-4 border-t border-gray-700 bg-[#1B1B1D]">
+    <footer className="w-full text-center text-gray-400 text-sm  py-4 border-t border-gray-700 bg-[#1B1B1D]">
       <div className="max-w-5xl mx-auto px-4  sm:flex-row items-center">
         <p className="text-xs sm:text-sm">
           © {new Date().getFullYear()} Abdelrahman Dawoud. Built with ❤️ using
